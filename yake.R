9ea2255a3308b40a5c451ad9a29a3104733d56c4
Jing Xu <jingxu21@bu.edu>
2023-04-13 13:04:43 -0400

    j

--- yake.R
+++ yake.R
@@ -1,53 +1,97 @@
-yake <- function(w, text){
+yake <- function(word, text){
 
 # get the casing
   ## function to return the number of times the word starts with a capital letter when it is not the beginning word of the sentence and the times when the word is in acronym form.
-  count_caps_and_acronyms <- function(text) {
-    words <- unlist(strsplit(text, "\\s+"))
-    cap_count <- 0
-    acronym_count <- 0
-    for (i in 2:length(words)) {
-      word <- words[i]
-      if (nchar(word) > 1) {
-        if (substr(word, 1, 1) == toupper(substr(word, 1, 1)) & 
-            substr(words[i-1], nchar(words[i-1]), nchar(words[i-1])) != ".") {
-          cap_count <- cap_count + 1
+  count_capital_acronym <- function(word, texts) {
+    
+    # Initialize variables for counting
+    count_capital = 0
+    count_acronym = 0
+    total_count = 0
+    
+    # Iterate over each text input
+    for (text in texts) {
+      
+      # Split text into sentences
+      sentences <- strsplit(text, "\\.\\s*")[[1]]
+      
+      # Iterate over each sentence
+      for (sentence in sentences) {
+        
+        # Split sentence into words
+        words <- strsplit(sentence, "\\s+")[[1]]
+        
+        # Iterate over each word
+        for (i in 2:length(words)) {
+          if (words[i] == word) {
+            
+            # Check if the word starts with a capital letter
+            if (substr(words[i], 1, 1) == toupper(substr(words[i], 1, 1))) {
+              count_capital <- count_capital + 1
+            }
+            
+            # Check if the word is an acronym
+            if (nchar(words[i]) > 1 && toupper(words[i]) == words[i]) {
+              count_acronym <- count_acronym + 1
+            }
+          }
         }
-        if (nchar(word) > 1 & toupper(word) == word) {
-          acronym_count <- acronym_count + 1
+      }
+      
+      # Count the total number of occurrences of the word
+      total_count <- total_count + length(grep(word, text, ignore.case = TRUE))
+    }
+    
+    # Calculate the final score
+    max_count <- max(count_capital, count_acronym)
+    score <- max_count / 1 + log(total_count)
+    
+    return(score)
+  }
+####
+# count_capital_acronym(word = 'cat cat', text = c('Cat Cat Cat', 'cat dog dog','cat CAT CAT'))
+####
+score <- count_capital_acronym(word = word, text = text)
+  
+# get the position
+position_feature <- function(word, text) {
+  
+  # Find sentence positions containing word
+  sen <- numeric()
+  for (i in seq_along(text)) {
+    sentences <- strsplit(text[i], "\\.\\s*")[[1]]
+    for (j in seq_along(sentences)) {
+      words <- strsplit(sentences[j], "\\s+")[[1]]
+      if (length(words) > 1) {
+        bigrams <- paste(words[-length(words)], words[-1], sep = " ")
+        if (word %in% bigrams) {
+          sen <- c(sen, j)
+        }
+      } else {
+        if (word == words) {
+          sen <- c(sen, j)
         }
       }
     }
-    max_count <- max(cap_count, acronym_count)
-    return(max_count)
   }
-  counts <- sapply(text, count_caps_and_acronyms)
-  total_count <- sum(sapply(text, function(x) length(strsplit(x, "\\s+")[[1]])))
-casing <- max(counts) / log(total_count)
-
-# get the position
-position_feature <- function(w,text) {
-  # Tokenize the input text into sentences
-  sentences <- sapply(text, function(x) {
-    strsplit(x, "[.?!]+\\s*")[[1]]
-  })
   
-  # Find the positions of sentences where the word "w" occurs
-  w_positions <- lapply(sentences, function(x) {
-    which(sapply(x, function(y) grepl("\\bw\\b", y)))
-  })
+  # Calculate position feature
+  if (length(sen) > 0) {
+    pos <- log(log(3 + median(sen)))
+  } else {
+    pos <- 0
+  }
   
-  # Compute the position feature for each text input
-  position <- sapply(w_positions, function(x) {
-    if (length(x) > 0) {
-      log(log(3 + median(x)))
-    } else {
-      NA
-    }
-  })
-  return(position)
+  return(pos)
 }
- position <-  position_feature(w,text)
+
+
+
+####
+ position_feature(word = 'cat', text = c( 'cat cat cat.','cat dog dog.'))
+####
+position <- position_feature(text = text, word = word)
+
 # get the word frequency 
   frequency_feature <- function(w, text) {
     # Tokenize the input text and create a vector of word counts
